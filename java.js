3a03fae7cd8b040d94d89725490a3d85d43c26c5
Jovit-Mathew236 <70875875+Jovit-Mathew236@users.noreply.github.com>
2020-10-15 23:07:33 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,47 @@
+function myClick(){
+    window.alert("https://jovit-mathew236.github.io/improve-std/")
+}
+
+function changeTheme2() {
+   var currentColor2 = document.getElementById("body2").style.backgroundColor;
+ 
+   
+      if (currentColor2 == "whitesmoke") {
+        document.getElementById("body2").style.backgroundColor = "black";
+        document.getElementById("box2").style.color = "white";
+        document.getElementById("table").style.borderColor = "thistle";
+        document.getElementById("table2").style.borderColor = "thistle";
+        document.getElementById("table3").style.borderColor = "thistle";
+        document.getElementById("table4").style.borderColor = "thistle";
+   } else {
+        document.getElementById("body2").style.backgroundColor = "whitesmoke";
+        document.getElementById("box2").style.color = "black";
+        document.getElementById("table").style.borderColor = "black";
+        document.getElementById("table2").style.borderColor = "black";
+        document.getElementById("table3").style.borderColor = "black";
+        document.getElementById("table4").style.borderColor = "black";
+   }
+}
+
+function changeTheme3() {
+     var currentColor2 = document.getElementById("body3").style.backgroundColor;
+   
+     
+        if (currentColor2 == "whitesmoke") {
+          document.getElementById("body3").style.backgroundColor = "black";
+     } else {
+          document.getElementById("body3").style.backgroundColor = "whitesmoke";
+     }
+  }
+
+function changeTheme4() {
+     var currentColor2 = document.getElementById("body4").style.backgroundColor;
+   
+     
+        if (currentColor2 == "whitesmoke") {
+          document.getElementById("body4").style.backgroundColor = "black";
+     } else {
+          document.getElementById("body4").style.backgroundColor = "whitesmoke";
+     }
+  }
+
